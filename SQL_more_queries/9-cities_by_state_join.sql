1b7329a7142e57f542a98bb00f39d2ba5882e546
michealchinedu1 <c.oragudosi@alustudent.com>
2023-08-13 08:17:23 -0700

    cities

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
---- lists all cities contained in the database hbtn_0d_usa
+-- lists all cities contained in the database hbtn_0d_usa
 -- lists all rows of a particular column in a database
 SELECT cities.id, cities.name, states.name 
       FROM cities 
